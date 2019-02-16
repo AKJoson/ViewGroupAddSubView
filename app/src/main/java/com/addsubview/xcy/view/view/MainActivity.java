a94ff85ee4a1bee0fc8ee7f5dfcce1494e0c43d0
yinpeng <yinpeng6868@gmail.com>
2019-02-16 09:33:26 +0800

    when you have been add some data,and then you want update those data.

--- app/src/main/java/com/addsubview/xcy/view/view/MainActivity.java
+++ app/src/main/java/com/addsubview/xcy/view/view/MainActivity.java
@@ -1,6 +1,8 @@
 package com.addsubview.xcy.view.view;
 
 import android.databinding.BindingAdapter;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -29,18 +31,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @BindingAdapter(value = {"subItem", "viewModel"}, requireAll = false)
-    public static void setSubItem(LinearLayout linearLayout, List<String> subItem,ViewModel viewModel) {
+    public static void setSubItem(LinearLayout linearLayout, List<String> subItem, ViewModel viewModel) {
         if (linearLayout != null) {
             //so you should clear the LinearLayout
             if (BaseUtil.isListEmpty(subItem))
                 linearLayout.removeAllViews();
             else {
-                for (int i = 0; i < subItem.size(); i++) {
-                    SubViewLayoutBinding binding = SubViewLayoutBinding.inflate(LayoutInflater.from(linearLayout.getContext()), linearLayout, false);
-                    binding.setSubItem(subItem.get(i));
-                    binding.setViewModel(viewModel);
-                    linearLayout.addView(binding.getRoot());
+                if (linearLayout.getChildCount() == 0) {
+                    //not add sub View ,first add
+                    for (int i = 0; i < subItem.size(); i++) {
+                        SubViewLayoutBinding binding = SubViewLayoutBinding.inflate(LayoutInflater.from(linearLayout.getContext()), linearLayout, false);
+                        binding.setSubItem(subItem.get(i));
+                        //you can not add the viewModel,because this is replace.not new create
+                        //  binding.setViewModel(viewModel);
+                        linearLayout.addView(binding.getRoot());
+                    }
+                } else if (linearLayout.getChildCount() > subItem.size()) {
+                    //you update data size less than now.
+                    for (int i = 0; i < subItem.size(); i++) {
+                        SubViewLayoutBinding binding = DataBindingUtil.getBinding(linearLayout.getChildAt(i));
+                        binding.setViewModel(viewModel);
+                        binding.setSubItem(subItem.get(i));
+                        binding.executePendingBindings();
+                    }
+                    linearLayout.removeViews(subItem.size(), linearLayout.getChildCount() - subItem.size());
+                } else {//you update data size more than now.
+                    int childCount = linearLayout.getChildCount();
+                    for (int i = 0; i < subItem.size(); i++) {
+                        if (i < childCount) {
+                            //replace current view data
+                            SubViewLayoutBinding binding = DataBindingUtil.getBinding(linearLayout.getChildAt(i));
+                            binding.setSubItem(subItem.get(i));
+                        } else {
+                            //add new view data
+                            SubViewLayoutBinding binding = SubViewLayoutBinding.inflate(LayoutInflater.from(linearLayout.getContext()), linearLayout, false);
+                            binding.setSubItem(subItem.get(i));
+                            binding.setViewModel(viewModel);
+                            linearLayout.addView(binding.getRoot());
+                        }
+                    }
                 }
+
             }
         }
     }
--- app/src/main/java/com/addsubview/xcy/view/viewmodel/ViewModel.java
+++ app/src/main/java/com/addsubview/xcy/view/viewmodel/ViewModel.java
@@ -9,13 +9,41 @@ import java.util.List;
 
 public class ViewModel extends BaseViewModel {
     public List<String> subItem;
+    /*
+    0 in current data to add new data,
+    1 update the forground three data .
+    2
+     */
+    private int status;
 
     public Runnable runnable = new Runnable() {
         @Override
         public void run() {
-            subItem = new ArrayList<>(10);
-            for (int i = 0; i < 10; i++) {
-                subItem.add("LinearLayout add sub view:" + i);
+            switch (status) {
+                case 0:
+                    subItem = new ArrayList<>(10);
+                    for (int i = 0; i < 10; i++) {
+                        subItem.add("LinearLayout add sub view:" + i);
+                    }
+                    ++status;
+                    break;
+                case 1:
+                    if (subItem == null)
+                        subItem = new ArrayList<>(3);
+                    subItem.clear();
+                    subItem.add("new data 1");
+                    subItem.add("new data 2");
+                    subItem.add("new data 3");
+                    ++status;
+                    break;
+                case 2:
+                    if (subItem == null)
+                        subItem = new ArrayList<>(20);
+                    subItem.clear();
+                    for (int i=0;i<20;i++){
+                        subItem.add("new Data："+i);
+                    }
+                    break;
             }
             notifyChange();
         }
@@ -24,6 +52,8 @@ public class ViewModel extends BaseViewModel {
     public ViewModel(AppCompatActivity activity) {
         super(activity);
         new Handler().postDelayed(runnable, 10000);//ten seconds.
+        new Handler().postDelayed(runnable, 15000);//15 seconds.
+        new Handler().postDelayed(runnable, 20000);//15 seconds.
     }
 
     public void itemClick(String itemValue) {
